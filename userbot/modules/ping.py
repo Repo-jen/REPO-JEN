69cfb1573e78c0ddf33f303da28826ddb3bd35d5
Zaen-userbot <91311626+Repo-jen@users.noreply.github.com>
2021-10-02 21:14:12 +0700

    Update ping.py

--- userbot/modules/ping.py
+++ userbot/modules/ping.py
@@ -70,7 +70,7 @@ async def redis(pong):
     await asyncio.sleep(1)
     end = datetime.now()
     duration = (end - start).microseconds / 1000
-    await pong.edit(f"**ZAEN!!!**\n**KEKUATAN ZAEN** : `%sms`\n**WAKTU BERMAIN ZAEN ** : `{uptime}🕛`" % (duration))
+    await pong.edit(f"**ZAEN!!!**\n**TUNJUKAN PESONAMU ZAEN** : `%sms`\n**WAKTU ZAEN BERMAIN ** : `{uptime}🕛`" % (duration))
 
 
 @register(outgoing=True, pattern="^Ping$")
